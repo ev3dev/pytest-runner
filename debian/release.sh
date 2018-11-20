36ef1415c9f32bbee0500bc12532a2cea8367355
David Lechner <david@lechnology.com>
2018-11-20 12:04:14 -0600

    debinan/release: fix binary package names
    also probably don't need environment variable

--- debian/release.sh
+++ debian/release.sh
@@ -4,8 +4,6 @@
 
 set -e
 
-export SKIP_CYTHON_BUILD=1
-
 source=$(dpkg-parsechangelog -S Source)
 version=$(dpkg-parsechangelog -S Version)
 distribution=$(dpkg-parsechangelog -S Distribution)
@@ -20,6 +18,6 @@ gbp buildpackage --git-tag-only
 ssh ev3dev@reprepro.ev3dev.org "reprepro -b ~/reprepro/raspbian includedsc ${codename} \
     ~/reprepro/debian/pool/main/${source:0:1}/${source}/${source}_${version}.dsc"
 ssh ev3dev@reprepro.ev3dev.org "reprepro -b ~/reprepro/raspbian includedeb ${codename} \
-    ~/reprepro/debian/pool/main/${source:0:1}/${source}/python-ptvsd_${version}_all.deb"
+    ~/reprepro/debian/pool/main/${source:0:1}/${source}/python-pytest-runner_${version}_all.deb"
 ssh ev3dev@reprepro.ev3dev.org "reprepro -b ~/reprepro/raspbian includedeb ${codename} \
-    ~/reprepro/debian/pool/main/${source:0:1}/${source}/python3-ptvsd_${version}_all.deb"
+    ~/reprepro/debian/pool/main/${source:0:1}/${source}/python3-pytest-runner_${version}_all.deb"
